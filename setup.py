52d919d97118c2271637154cd8038b4101a477c6
Francesco Bruschi <fbrusch@gmail.com>
2013-01-24 15:09:39 +0100

    introduced a limit on the number of active threads

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "pmap",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Francesco Bruschi",
     py_modules = ["pmap"],
     description = "a multithreaded map operator",
